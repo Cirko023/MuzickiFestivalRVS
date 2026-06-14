3d3f35e8274b23d71fe51e9cd7b9a4bfbc1c62d3
Cirko023 <ciricjovan27@gmail.com>
2026-06-14 19:45:11 +0200

    Dodata klasa CitacPravila i XML fajl PravilaPopusta u Sloju Poslovne logike

---
+++ SlojPoslovneLogike/Ogranicenja/CitacPravila.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http.Json;
+
+namespace SlojPoslovneLogike.Ogranicenja;
+
+public class CitacPravila
+{
+    private readonly HttpClient _httpClient;
+    private const string UrlServisa = "http://localhost:5072/api/PravilaRest";
+
+    public CitacPravila(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<decimal> DohvatiProcenatPopusta()
+    {
+        var pravila = await _httpClient.GetFromJsonAsync<PravilaModel>(UrlServisa);
+        return pravila?.ProcenatPopusta ?? 10;
+    }
+}
+
+public class PravilaModel
+{
+    public decimal ProcenatPopusta { get; set; }
+    public string Opis { get; set; } = string.Empty;
+}

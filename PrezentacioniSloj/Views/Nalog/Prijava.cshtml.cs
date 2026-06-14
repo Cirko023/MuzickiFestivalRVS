c1823728ab6a5bfb6fdf6cd8165bfac4668282ac
Cirko023 <ciricjovan27@gmail.com>
2026-06-14 23:34:50 +0200

    Dodata Stranica za prijavu korisnika

---
+++ PrezentacioniSloj/Views/Nalog/Prijava.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PrezentacioniSloj.Views.Nalog
+{
+    public class PrijavaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

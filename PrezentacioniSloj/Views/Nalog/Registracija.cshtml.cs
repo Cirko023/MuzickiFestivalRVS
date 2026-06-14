f305e5bbdf7e9163d8e959b89abf9da348018fcd
Cirko023 <ciricjovan27@gmail.com>
2026-06-14 23:54:16 +0200

    Dodata stranica za registraciju korisnika.

---
+++ PrezentacioniSloj/Views/Nalog/Registracija.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PrezentacioniSloj.Views.Nalog
+{
+    public class RegistracijaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

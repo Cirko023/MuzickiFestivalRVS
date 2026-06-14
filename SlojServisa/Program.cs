c8907fa85c2b80aff98116a67b9975ec2109caf0
Cirko023 <ciricjovan27@gmail.com>
2026-06-14 22:44:53 +0200

    Izmena Program.cs u SlojServisu

--- SlojServisa/Program.cs
+++ SlojServisa/Program.cs
@@ -1,29 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using SlojPodataka.Repozitorijum;
+using SlojPodataka.TehnoloskeKlase;
+using SlojPoslovneLogike.Ogranicenja;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<FestivalDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("PodrazumevanaKonekcija")));
+
+Konekcija.NizKonekcije = builder.Configuration.GetConnectionString("PodrazumevanaKonekcija")!;
+
+builder.Services.AddScoped<ZapisnikKoncertaRepozitorijum>();
+builder.Services.AddScoped<KorisnikRepozitorijum>();
+builder.Services.AddScoped<IzvodjacRepozitorijum>();
+builder.Services.AddScoped<IzvodjacRepoDBUtils>();
+builder.Services.AddHttpClient();
+builder.Services.AddScoped<CitacPravila>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
 
-app.UseHttpsRedirection();
 app.UseRouting();
-
 app.UseAuthorization();
-
-app.MapStaticAssets();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
-
+app.MapControllers();
 app.Run();

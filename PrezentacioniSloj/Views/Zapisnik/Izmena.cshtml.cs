44e57fd927b1d09c0b8ec021d7e32aba6cb65cad
Cirko023 <ciricjovan27@gmail.com>
2026-06-15 16:46:32 +0200

    Dodata stranica za izmenu

---
+++ PrezentacioniSloj/Views/Zapisnik/Izmena.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PrezentacioniSloj.Views.Zapisnik
+{
+    public class IzmenaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

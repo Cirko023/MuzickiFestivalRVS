59a95314da18312676ad2fd3cb3b89219667c537
Cirko023 <ciricjovan27@gmail.com>
2026-06-15 16:59:17 +0200

    Dodata stranica za stampu.

---
+++ PrezentacioniSloj/Views/Zapisnik/Stampa.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PrezentacioniSloj.Views.Zapisnik
+{
+    public class StampaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

1d8719ded3bf4742024b6cda5063e408aaa6e61a
Cirko023 <ciricjovan27@gmail.com>
2026-06-15 17:15:28 +0200

    Dodat View Model za prijavu

---
+++ PrezentacioniSloj/PrezentacionaLogika/ViewModeli/PrijavaViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace PrezentacioniSloj.PrezentacionaLogika.ViewModeli;
+
+public class PrijavaViewModel
+{
+    [Required(ErrorMessage = "Korisničko ime je obavezno.")]
+    public string KorisnickoIme { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Lozinka je obavezna.")]
+    [DataType(DataType.Password)]
+    public string Lozinka { get; set; } = string.Empty;
+}

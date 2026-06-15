6aa91898eca8f5f2713a116472541003daed6cd4
Cirko023 <ciricjovan27@gmail.com>
2026-06-15 17:06:36 +0200

    Dodat Stavka Pesme View Model

---
+++ PrezentacioniSloj/PrezentacionaLogika/ViewModeli/StavkaPesmeViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PrezentacioniSloj.PrezentacionaLogika.ViewModeli;
+
+public class StavkaPesmeViewModel
+{
+    public int StavkaID { get; set; }
+
+    [Required(ErrorMessage = "Redni broj je obavezan.")]
+    [Range(1, 50, ErrorMessage = "Redni broj mora biti između 1 i 50.")]
+    public int RedniBroj { get; set; }
+
+    [Required(ErrorMessage = "Naziv pesme je obavezan.")]
+    [StringLength(150, MinimumLength = 2, ErrorMessage = "Naziv pesme mora imati 2-150 karaktera.")]
+    public string NazivPesme { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Trajanje je obavezno.")]
+    [Range(1, 30, ErrorMessage = "Trajanje mora biti između 1 i 30 minuta.")]
+    public int TrajanjeMinute { get; set; }
+}
